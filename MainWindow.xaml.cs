3c918dd8ae9652e78005a9dd9c1686431f21681b
JoshDegazio <36270908+JoshDegazio@users.noreply.github.com>
2019-02-06 15:08:29 -0500

    Added Files
    Crab rave time

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿/* Josh Degazio
+ * Feb 6, 2019
+ * Hello World my "first" program
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _182685HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string name = input_name.Text;
+            MessageBox.Show("Hey " + name + ", thanks for checking out my program.");
+            MessageBox.Show("Sike, you just got ebola.");
+            message.Content = "Hello, " + name + ", and welcome.";
+        }
+    }
+}
